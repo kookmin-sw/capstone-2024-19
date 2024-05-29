e954f40379570b752e0ff16b858156069f33fa31
minseokey <lms990427@kookmin.ac.kr>
2024-05-29 12:48:53 +0900

    Feat: anonymous token

--- composite-service/src/main/java/com/jumunseo/compositeservice/query/controller/CommunityQueryController.java
+++ composite-service/src/main/java/com/jumunseo/compositeservice/query/controller/CommunityQueryController.java
@@ -126,9 +126,13 @@ public class CommunityQueryController {
     @Tag(name = "Community Query")
     @Operation(summary = "Community Detail", description = "커뮤니티 상세 조회")
     private ResponseEntity<Result<?>> getCommunityDetail(@PathVariable Long id, HttpServletRequest req) throws JsonProcessingException {
+        String email = req.getAttribute("email").toString();
+        if (email == null) {
+            email = "UNSIGNED";
+        }
         Mono<JSONObject> mono = webClient.get()
                 .uri(COMMUNITY_URL +"/community/board/detail/" + id)
-                .header("email", req.getAttribute("email").toString())
+                .header("email", email)
                 .retrieve()
                 // 4-- 에러 -> 요청 오류
                 .onStatus(HttpStatusCode::is4xxClientError, res -> Mono.error(
