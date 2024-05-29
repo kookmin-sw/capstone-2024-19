f8acd19aace949eebcb328e1c7873b60b4cae522
minseokey <lms990427@kookmin.ac.kr>
2024-05-29 13:09:39 +0900

    Feat: check like api

--- composite-service/src/main/java/com/jumunseo/compositeservice/query/controller/CommunityQueryController.java
+++ composite-service/src/main/java/com/jumunseo/compositeservice/query/controller/CommunityQueryController.java
@@ -172,6 +172,28 @@ public class CommunityQueryController {
         }
     }
 
+    // 좋아요 여부 조회
+    // 토큰 필요
+    @GetMapping("/like/{board_id}")
+    @Tag(name = "Community Query")
+    @Operation(summary = "Like Check", description = "좋아요 여부 조회")
+    private ResponseEntity<Result<?>> getLike(HttpServletRequest req, @PathVariable Long board_id) {
+        Mono<JSONObject> mono = webClient.get()
+                .uri(COMMUNITY_URL +"/community/board/like/" + board_id)
+                .header("email", req.getAttribute("email").toString())
+                .retrieve()
+                // 4-- 에러 -> 요청 오류
+                .onStatus(HttpStatusCode::is4xxClientError, res -> Mono.error(
+                        new WebClient4xxException(res.bodyToMono(String.class).toString())
+                ))
+                // 5-- 에러 -> 시스템 오류
+                .onStatus(HttpStatusCode::is5xxServerError, res -> Mono.error(
+                        new WebClient5xxException(res.bodyToMono(String.class).toString())
+                ))
+                .bodyToMono(JSONObject.class);
+        return ResponseEntity.ok(Result.successResult(mono.block().get("data")));
+    }
+
 
     // 댓글 리스트 조회
     // 토큰 필요 X
